086f1e4fc6a2dc14fc9df2f682a25841e20c973d
Foxsor <tomasbaril@gmail.com>
2022-03-21 23:01:06 -0300

    add .env

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import logo from "./logo.svg";
+import React, { useState, useEffect }  from 'react';
 import "./App.css";
   
-function App() {
+const App = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+      fetch(process.env.REACT_APP_API_URL)
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    setIsLoaded(true);
+                    setUsers(data);
+                },
+            )
+      }, [])
+
+      console.log(users)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" 
              alt="logo" />
-        <p>A simple React app.....</p>
+        <p>A simple React app..... </p>
         <a
           className="App-link"
           href="https://reactjs.org"
